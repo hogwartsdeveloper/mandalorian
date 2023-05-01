90f1b290a2669790387a37735c1a475285d591b5
zhannur <jannuraidynuly@gmail.com>
2023-05-01 07:47:07 +0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import * as THREE from 'three';
-import {skyFragmentShader, skyVertexShader} from "./models/three.model";
-import {PlayerService} from "./services/player.service";
-import {WorldManagerService} from "./services/world-manager.service";
+import {pcss, pcssGetShadow, skyFragmentShader, skyVertexShader} from "./models/three.model";
 
 @Component({
   selector: 'app-root',
@@ -14,15 +12,16 @@ export class AppComponent implements AfterViewInit {
   renderer: THREE.WebGLRenderer;
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(
-      30,
+      60,
       window.innerWidth / window.innerHeight,
       1,
-      5000
+      20000
   );
-  hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
-  dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  clock = new THREE.Clock();
+  gameStarted = false;
+  gameOver = false;
+  previousTime: number;
   score = '00000'
+
   @HostListener('window:resize', ['$event'])
   resize() {
     this.camera.aspect = window.innerWidth / window.innerHeight;
@@ -31,104 +30,114 @@ export class AppComponent implements AfterViewInit {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  constructor(
-      private playerService: PlayerService,
-      private worldManager: WorldManagerService
-  ) {}
+  constructor() {}
   ngAfterViewInit() {
-    this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvas.nativeElement });
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.shadowMap.enabled = true;
-
-    this.scene.background = new THREE.Color().setHSL(0.6, 0, 1);
-    this.scene.fog = new THREE.Fog(this.scene.background, 1, 5000);
-
-    this.camera.position.set(0, 0, 250);
-
+    this.init();
     this.setLight();
     this.setGround();
-    this.setSky();
-    this.playerService.init(this.scene);
+    this.setSky()
     this.animate();
   }
 
+  init() {
+    let shadowCode = THREE.ShaderChunk.shadowmap_pars_fragment;
+    shadowCode = shadowCode.replace(
+        '#ifdef USE_SHADOWMAP',
+        '#ifdef USE_SHADOWMAP' + pcss
+    );
+    shadowCode = shadowCode.replace(
+        '#if defined( SHADOWMAP_TYPE_PCF )',
+        pcssGetShadow +
+        '#if defined( SHADOWMAP_TYPE_PCF )'
+    );
+
+    THREE.ShaderChunk.shadowmap_pars_fragment = shadowCode;
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvas.nativeElement });
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+    this.camera.position.set(-5, 5, 10);
+    this.camera.lookAt(8, 3, 0);
+
+    this.scene.background = new THREE.Color(0x808080);
+    this.scene.fog = new THREE.FogExp2(0x89b2eb, 0.00125);
+  }
+
   setLight() {
-    this.hemiLight.color.setHSL( 0.6, 1, 0.6 );
-    this.hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-    this.hemiLight.position.set( 0, 50, 0 );
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(60, 100, 10);
+    light.target.position.set(40, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 4096;
+    light.shadow.mapSize.height = 4096;
+    light.shadow.camera.far = 200.0;
+    light.shadow.camera.near = 1.0;
+    light.shadow.camera.left = 50;
+    light.shadow.camera.right = -50;
+    light.shadow.camera.top = 50;
+    light.shadow.camera.bottom = -50;
+    this.scene.add(light);
 
-    this.scene.add(this.hemiLight);
-
-    this.dirLight.color.setHSL( 0.1, 1, 0.95 );
-    this.dirLight.position.set( - 1, 1.75, 1 );
-    this.dirLight.position.multiplyScalar( 30 );
-    this.scene.add( this.dirLight );
-
-    this.dirLight.castShadow = true;
-
-    this.dirLight.shadow.mapSize.width = 2048;
-    this.dirLight.shadow.mapSize.height = 2048;
-
-    const d = 50;
-
-    this.dirLight.shadow.camera.left = - d;
-    this.dirLight.shadow.camera.right = d;
-    this.dirLight.shadow.camera.top = d;
-    this.dirLight.shadow.camera.bottom = - d;
-
-    this.dirLight.shadow.camera.far = 3500;
-    this.dirLight.shadow.bias = - 0.0001;
+    this.scene.add(
+        new THREE.HemisphereLight(0x202020, 0x004080, 0.6)
+    )
   }
 
   setGround() {
-    const groundGeo = new THREE.PlaneGeometry(10000, 10000);
-    const groundMat = new THREE.MeshLambertMaterial({color: 0xffffff });
-    groundMat.color.setHSL( 0.095, 1, 0.75 );
-
-    const ground = new THREE.Mesh(groundGeo, groundMat);
-    ground.position.y = -33;
-    ground.rotation.x = - Math.PI / 2;
+    const ground = new THREE.Mesh(
+        new THREE.PlaneGeometry(20000, 20000, 10, 10),
+        new THREE.MeshStandardMaterial({
+          color: 0xf6f47f,
+        }));
+    ground.castShadow = false;
     ground.receiveShadow = true;
+    ground.rotation.x = -Math.PI / 2;
     this.scene.add(ground);
   }
 
   setSky() {
     const uniforms = {
-      'topColor': { value: new THREE.Color( 0x0077ff ) },
-      'bottomColor': { value: new THREE.Color( 0xffffff ) },
-      'offset': { value: 33 },
-      'exponent': { value: 0.6 }
+      topColor: { value: new THREE.Color(0x0077FF) },
+      bottomColor: { value: new THREE.Color(0x89b2eb) },
+      offset: { value: 33 },
+      exponent: { value: 0.6 }
     };
-    uniforms[ 'topColor' ].value.copy( this.hemiLight.color );
 
-    this.scene.fog!.color.copy( uniforms[ 'bottomColor' ].value );
-
-    const skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
-    const skyMat = new THREE.ShaderMaterial( {
+    const skyGeo = new THREE.SphereGeometry(1000, 32, 15);
+    const skyMat = new THREE.ShaderMaterial({
       uniforms: uniforms,
       vertexShader: skyVertexShader,
       fragmentShader: skyFragmentShader,
-      side: THREE.BackSide
-    } );
+      side: THREE.BackSide,
+    });
 
-    const sky = new THREE.Mesh( skyGeo, skyMat );
-    this.scene.add( sky );
+    this.scene.add(new THREE.Mesh(skyGeo, skyMat));
+  }
+
+  onStart() {
+    this.gameStarted = true;
   }
 
   animate() {
-    const animation = () => {
-      this.render();
+    const animation = (time: number) => {
+      if (!this.previousTime) {
+        this.previousTime = time;
+      }
+      this.step((time - this.previousTime) / 1000);
+      this.renderer.render(this.scene, this.camera);
+      this.previousTime = time;
     }
 
     this.renderer.setAnimationLoop(animation);
   }
 
-  render() {
-    const delta = this.clock.getDelta();
-    this.playerService.update(delta);
-    this.worldManager.update(this.scene, delta);
-    this.score = this.worldManager.updateScore(delta);
-    this.renderer.render(this.scene, this.camera);
+  step(timeElapsed: number) {
+    if (this.gameOver || !this.gameStarted) {
+      return
+    }
   }
 }
--- src/app/models/three.model.ts
+++ src/app/models/three.model.ts
@@ -1,22 +1,93 @@
 export const skyVertexShader = `
-    varying vec3 vWorldPosition;
-    void main() {
-        vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-        vWorldPosition = worldPosition.xyz;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    }`;
+varying vec3 vWorldPosition;
+void main() {
+  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+  vWorldPosition = worldPosition.xyz;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}`;
 
 export const skyFragmentShader = `
-    uniform vec3 topColor;
-    uniform vec3 bottomColor;
-    uniform float offset;
-    uniform float exponent;
+uniform vec3 topColor;
+uniform vec3 bottomColor;
+uniform float offset;
+uniform float exponent;
+varying vec3 vWorldPosition;
+void main() {
+  float h = normalize( vWorldPosition + offset ).y;
+  gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
+}`;
 
-    varying vec3 vWorldPosition;
+export const pcss = `
+#define LIGHT_WORLD_SIZE 0.05
+#define LIGHT_FRUSTUM_WIDTH 3.75
+#define LIGHT_SIZE_UV (LIGHT_WORLD_SIZE / LIGHT_FRUSTUM_WIDTH)
+#define NEAR_PLANE 1.0
+#define NUM_SAMPLES 17
+#define NUM_RINGS 11
+#define BLOCKER_SEARCH_NUM_SAMPLES NUM_SAMPLES
+#define PCF_NUM_SAMPLES NUM_SAMPLES
+vec2 poissonDisk[NUM_SAMPLES];
+void initPoissonSamples( const in vec2 randomSeed ) {
+  float ANGLE_STEP = PI2 * float( NUM_RINGS ) / float( NUM_SAMPLES );
+  float INV_NUM_SAMPLES = 1.0 / float( NUM_SAMPLES );
+  // jsfiddle that shows sample pattern: https://jsfiddle.net/a16ff1p7/
+  float angle = rand( randomSeed ) * PI2;
+  float radius = INV_NUM_SAMPLES;
+  float radiusStep = radius;
+  for( int i = 0; i < NUM_SAMPLES; i ++ ) {
+    poissonDisk[i] = vec2( cos( angle ), sin( angle ) ) * pow( radius, 0.75 );
+    radius += radiusStep;
+    angle += ANGLE_STEP;
+  }
+}
+float penumbraSize( const in float zReceiver, const in float zBlocker ) { // Parallel plane estimation
+  return (zReceiver - zBlocker) / zBlocker;
+}
+float findBlocker( sampler2D shadowMap, const in vec2 uv, const in float zReceiver ) {
+  // This uses similar triangles to compute what
+  // area of the shadow map we should search
+  float searchRadius = LIGHT_SIZE_UV * ( zReceiver - NEAR_PLANE ) / zReceiver;
+  float blockerDepthSum = 0.0;
+  int numBlockers = 0;
+  for( int i = 0; i < BLOCKER_SEARCH_NUM_SAMPLES; i++ ) {
+    float shadowMapDepth = unpackRGBAToDepth(texture2D(shadowMap, uv + poissonDisk[i] * searchRadius));
+    if ( shadowMapDepth < zReceiver ) {
+      blockerDepthSum += shadowMapDepth;
+      numBlockers ++;
+    }
+  }
+  if( numBlockers == 0 ) return -1.0;
+  return blockerDepthSum / float( numBlockers );
+}
+float PCF_Filter(sampler2D shadowMap, vec2 uv, float zReceiver, float filterRadius ) {
+  float sum = 0.0;
+  for( int i = 0; i < PCF_NUM_SAMPLES; i ++ ) {
+    float depth = unpackRGBAToDepth( texture2D( shadowMap, uv + poissonDisk[ i ] * filterRadius ) );
+    if( zReceiver <= depth ) sum += 1.0;
+  }
+  for( int i = 0; i < PCF_NUM_SAMPLES; i ++ ) {
+    float depth = unpackRGBAToDepth( texture2D( shadowMap, uv + -poissonDisk[ i ].yx * filterRadius ) );
+    if( zReceiver <= depth ) sum += 1.0;
+  }
+  return sum / ( 2.0 * float( PCF_NUM_SAMPLES ) );
+}
+float PCSS ( sampler2D shadowMap, vec4 coords ) {
+  vec2 uv = coords.xy;
+  float zReceiver = coords.z; // Assumed to be eye-space z in this code
+  initPoissonSamples( uv );
+  // STEP 1: blocker search
+  float avgBlockerDepth = findBlocker( shadowMap, uv, zReceiver );
+  //There are no occluders so early out (this saves filtering)
+  if( avgBlockerDepth == -1.0 ) return 1.0;
+  // STEP 2: penumbra size
+  float penumbraRatio = penumbraSize( zReceiver, avgBlockerDepth );
+  float filterRadius = penumbraRatio * LIGHT_SIZE_UV * NEAR_PLANE / zReceiver;
+  // STEP 3: filtering
+  //return avgBlockerDepth;
+  return PCF_Filter( shadowMap, uv, zReceiver, filterRadius );
+}
+`;
 
-    void main() {
-
-    float h = normalize( vWorldPosition + offset ).y;
-    gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
-
-    }`;
+export const pcssGetShadow = `
+return PCSS( shadowMap, shadowCoord );
+`;
